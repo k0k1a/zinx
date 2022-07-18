8d7e376acb64e0174158f20ba6de4258737f3763
song <1964442868@qq.com>
2022-07-18 11:46:33 +0800

    init & server

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/k0k1a/zinx
+
+go 1.17
+
+replace (
+	github.com/k0k1a/zinx/ziface => ../zinx/ziface
+	github.com/k0k1a/zinx/znet => ../zinx/znet
+)
---
+++ znet/Server.go
@@ -0,0 +1,89 @@
+package znet
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/k0k1a/zinx/ziface"
+)
+
+//IServer 接口的实现
+type Server struct {
+	//服务器的名称
+	Name string
+	//服务器绑定的IP版本
+	IPVersion string
+	//服务器监听的IP
+	IP string
+	//服务器监听的端口
+	Port int
+}
+
+func (s *Server) Start() {
+	fmt.Printf("[Start] Server Listenner at IP :%s,Port :%d, is starting", s.IP, s.Port)
+
+	go func() {
+		//1.获取TCP Addr
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
+		if err != nil {
+			fmt.Println("resolve tcp addr error", err)
+			return
+		}
+		//2.监听服务器地址
+		listenner, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listen", s.IPVersion, "err", err)
+			return
+		}
+
+		fmt.Println("start Zinx Serer succeed", s.Name, "succ,listenning...")
+
+		//3.阻塞的等待连接过来，处理客户端业务
+		for {
+			//如果有客户端连接过来，阻塞回返回
+			conn, err := listenner.AcceptTCP()
+			if err != nil {
+				fmt.Println("Accept err", err)
+				continue
+			}
+
+			go func() {
+				for {
+					buf := make([]byte, 512)
+					cnt, err := conn.Read(buf)
+					if err != nil {
+						fmt.Println("read buf err", err)
+						continue
+					}
+					if _, err := conn.Write(buf[:cnt]); err != nil {
+						fmt.Println("write back buf err", err)
+						continue
+					}
+				}
+			}()
+		}
+	}()
+
+}
+
+func (s *Server) Stop() {
+	//TODO
+}
+
+func (s *Server) Serve() {
+	s.Start()
+
+	//阻塞状态
+	select {}
+}
+
+func New(name string) ziface.IServer {
+
+	s := &Server{
+		Name:      name,
+		IPVersion: "tcp4",
+		IP:        "0.0.0.0",
+		Port:      8999,
+	}
+	return s
+}
